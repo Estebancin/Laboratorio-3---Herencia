7cab5caa6451b6d600e9d8b233bec37906dfd1bf
Dhastry <139798946+Dhastry@users.noreply.github.com>
2023-10-23 19:35:59 -0600

    Update Oficina.java

--- Oficina.java
+++ Oficina.java
@@ -1,8 +1,26 @@
+/**
+ * La clase Oficina representa un espacio de tipo "Oficina" en el edificio.
+ * Esta clase extiende la clase Espacio y agrega atributos adicionales
+ * específicos para las oficinas, como el número total de parqueos y si
+ * incluye mantenimiento.
+ *
+ * @author Esteban Ramírez y Dhastry Secaira 
+ */
 public class Oficina extends Espacio {
-    private int totalParqueos;
-    private boolean mantenimiento;
+    private int totalParqueos; 
+    private boolean mantenimiento; 
 
-    // Constructor
+    /**
+     * Constructor de la clase Oficina.
+     *
+     * @param id Identificador único del espacio.
+     * @param metrosCuadrados Tamaño del espacio en metros cuadrados.
+     * @param cantidadDisponible Número de unidades disponibles para la venta o el alquiler.
+     * @param cantidadVendidos Número de unidades que ya han sido vendidas o alquiladas.
+     * @param estado Estado actual del espacio (disponible, reservado, etc.).
+     * @param totalParqueos Número total de parqueos asignados a la oficina.
+     * @param mantenimiento Indica si la oficina incluye mantenimiento.
+     */
     public Oficina(int id, double metrosCuadrados, int cantidadDisponible, int cantidadVendidos, String estado,
                    int totalParqueos, boolean mantenimiento) {
         super(id, metrosCuadrados, cantidadDisponible, cantidadVendidos, estado);
@@ -10,20 +28,39 @@ public class Oficina extends Espacio {
         this.mantenimiento = mantenimiento;
     }
 
-   // Getters y setters para cada atributo adicional
-   public int getTotalParqueos() {
-       return totalParqueos;
-   }
+    /**
+     * Obtiene el número total de parqueos asignados a la oficina.
+     *
+     * @return Número total de parqueos asignados a la oficina.
+     */
+    public int getTotalParqueos() {
+        return totalParqueos;
+    }
 
-   public void setTotalParqueos(int totalParqueos) {
-       this.totalParqueos = totalParqueos;
-   }
+    /**
+     * Establece el número total de parqueos asignados a la oficina.
+     *
+     * @param totalParqueos Número total de parqueos asignados a la oficina.
+     */
+    public void setTotalParqueos(int totalParqueos) {
+        this.totalParqueos = totalParqueos;
+    }
 
-   public boolean isMantenimiento() {
-       return mantenimiento;
-   }
+    /**
+     * Verifica si la oficina incluye mantenimiento.
+     *
+     * @return true si la oficina incluye mantenimiento, false en caso contrario.
+     */
+    public boolean isMantenimiento() {
+        return mantenimiento;
+    }
 
-   public void setMantenimiento(boolean mantenimiento) {
-       this.mantenimiento = mantenimiento;
-   }
+    /**
+     * Establece si la oficina incluye mantenimiento.
+     *
+     * @param mantenimiento true si la oficina incluye mantenimiento, false en caso contrario.
+     */
+    public void setMantenimiento(boolean mantenimiento) {
+        this.mantenimiento = mantenimiento;
+    }
 }
